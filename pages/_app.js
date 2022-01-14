5ebe2fef37736787dee5f904d5b5421901b682c5
Shubham Panchal <panchalshubham@826gmail.com>
2022-01-14 14:45:04 +0530

    Added base code

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,15 @@
 import '../styles/globals.css'
+import { Router } from 'next/dist/client/router';
+import ProgressBar from '@badrap/bar-of-progress';
+const progress = new ProgressBar({
+  size:6,
+  color:"#3f51b5",
+  className:"z-50",
+  delay:100,
+})
+Router.events.on("routeChangeStart",progress.start)
+Router.events.on("routeChangeComplete",progress.finish)
+Router.events.on("routeChangeError",progress.finish)
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
