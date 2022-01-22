8911fa70226967cd632c115bae977f438e641ecb
Shubham Panchal <panchalshubham@826gmail.com>
2022-01-22 18:22:21 +0530

    fixed: Error: ' can be escaped blablablabla....so on

--- pages/404.js
+++ pages/404.js
@@ -7,7 +7,7 @@ export default function NotFound() {
       <Header />
       <div className="absolute top-0 left-0 right-0 bottom-0 m-auto text-center w-[300spx] h-[100px]">
         <div className="text-4xl font-thin lg:text-5xl">Thanks for your interest. </div>
-        <div className="text-3xl font-thin lg:text-4xl">We're working on it.</div>
+        <div className="text-3xl font-thin lg:text-4xl">We&apos;re working on it.</div>
         <br />
         <button
           className="text-[#3f51b5] font-bold uppercase text-sm px-6 py-3 mr-1 mb-1 ease-linear transition-all duration-150"
