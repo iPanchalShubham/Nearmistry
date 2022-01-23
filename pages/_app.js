9e38139778e87849eff8267ed82b3470b0b6f75c
Shubham Panchal <panchalshubham@826gmail.com>
2022-01-23 22:32:35 +0530

    Fixed some minor issues with the UI

--- pages/_app.js
+++ pages/_app.js
@@ -1,18 +1,18 @@
 import '../styles/globals.css'
 import { Router } from 'next/dist/client/router';
 import ProgressBar from '@badrap/bar-of-progress';
+
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
 const progress = new ProgressBar({
-  size:2,
-  color:"#FBFBFD",
+  size:2.5,
+  color:"white",
   className:"z-50",
   delay:100,
 })
 Router.events.on("routeChangeStart",progress.start)
 Router.events.on("routeChangeComplete",progress.finish)
 Router.events.on("routeChangeError",progress.finish)
-
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
 
 export default MyApp
