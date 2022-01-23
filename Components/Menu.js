6b2baaa0370ba4507dee124f77a9504c842c5d0b
Shubham Panchal <panchalshubham@826gmail.com>
2022-01-23 16:10:05 +0530

    Added sidebar and a backdrop, coupled with some nice background blur effect(+fixed some bugs)

---
+++ Components/Menu.js
@@ -0,0 +1,35 @@
+import { ReactDimmer } from "react-dimmer";
+import { useRouter } from "next/router";
+const Menu = ({ MenuHandler, showSidebar }) => {
+  const router = useRouter();
+  return (
+    <>
+      <ReactDimmer
+        isOpen={showSidebar}
+        exitDimmer={MenuHandler}
+        onClick={MenuHandler}
+        zIndex={30}
+        blur={1.5}
+      />
+      <div
+        className={`top-0 left-0 bg-gray-100 p-10 pr-15 lg:pr-20 fixed h-full z-40  ease-out duration-100 ${
+          showSidebar ? "translate-x-0" : "translate-x-[-100%]"
+        }`}
+      >
+        <div
+          className="mt-20 space-y-3  mr-4 font-extralight text-xl"
+          onClick={() => router.push(`/404.js`)}
+        >
+          <h4 className="cursor-pointer">Why us?</h4>
+          <h4 className="cursor-pointer">Become a Volunteer</h4>
+          <h4 className="cursor-pointer">How it works?</h4>
+          <h4 className="cursor-pointer">Contact us</h4>
+          <h4 className="cursor-pointer">About us</h4>
+          <h4 className="cursor-pointer">Help us</h4>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Menu;
