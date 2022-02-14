db5985a37ec7d222f303e56c92e72c2b0a03e659
Shubham Panchal <panchalshubham826@gmail.com>
2022-02-14 12:36:57 +0530

    Added env varibles inside next.config.js that should fix the failing check

--- next.config.js
+++ next.config.js
@@ -1,3 +1,9 @@
 module.exports = {
   reactStrictMode: true,
+  env: {
+    MONGODB_URI = process.env.MONGODB_URI,
+NEXT_PUBLIC_API_TOKEN =process.env.NEXT_PUBLIC_API_TOKEN,
+GET_OCCUPATION_ARRAY_API = process.env.GET_OCCUPATION_ARRAY_API,
+MAIN_DATA_GETTING_API=process.env.MAIN_DATA_GETTING_API,
+  }
 }
