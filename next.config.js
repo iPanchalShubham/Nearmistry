a9781968024dddcfb80941991b6d7ea8c60b26cb
Shubham Panchal <panchalshubham826@gmail.com>
2022-02-14 13:11:53 +0530

    Bug fixes inside next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 const nextConfig = {
   env: {
-NEXT_PUBLIC_API_TOKEN :process.env.API_TOKEN,
+API_TOKEN :process.env.API_TOKEN,
 GET_OCCUPATION_ARRAY_API : process.env.GET_OCCUPATION_ARRAY_API,
 MAIN_DATA_GETTING_API:process.env.MAIN_DATA_GETTING_API,
   }
