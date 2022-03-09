068fda03eb27cfa5fc2fea229f539422b0cf5437
Shubham Panchal <panchalshubham826@gmail.com>
2022-03-09 08:59:12 +0530

    Made some changes in about.jsx

--- Components/Static components/About.jsx
+++ Components/Static components/About.jsx
@@ -9,7 +9,7 @@ function About() {
       <Header />
       <div className="h-[100vh] ">
         <div className="flex justify-center flex-col m-3 items-center">
-          <div className="font-medium bg-[#a8cbff] text-3xl s:text-4xl l:text-5xl m-4 lg:m-5 underline rounded py-1 px-2 inline-block ">
+          <div className="font-medium bg-[#a8cbff] text-3xl l:text-5xl m-4 lg:m-5 underline rounded py-1 px-2 inline-block ">
             About Nearmistry
           </div>
           <Image src={mistrisGroup} objectFit="contain" />
@@ -19,16 +19,16 @@ function About() {
             </blockquote>
           </p>
           <div className="text-right">– Andre Gide.</div>
-          <div className="font-medium text-2xl m-4 bg-[#a3c4f5] lg:my-4 underline rounded py-1 px-2 inline-block ">
+          <div className="text-2xl m-4 bg-[#a9c6f1] lg:my-4 underline rounded py-1 px-2 inline-block ">
             Our Vision
           </div>
-          <p className="text-xl mt-3 mb-3 font-semibold lg:max-w-4xl text-center">
+          <span className="text-xl mt-3 mb-3 font-medium lg:font-semibold max-w-4xl text-center">
             <span className="text-2xl font-bold">O</span>
             ur vision is to bring every street-smart skilled person online, so
             that hiring could be done efficiently. Our goal is to develop
             Nearmistry to a futuristic platform where anyone can hire skilled
             people. So that, skilled people can make their living easily.
-          </p>
+          </span>
           <figcaption>—Shubham Panchal, <cite>Founder at NearMistry.</cite></figcaption>
         </div>
       </div>
