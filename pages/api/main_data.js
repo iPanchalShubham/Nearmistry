f17e33f39aac406602ad5c96d0bba07623ba4502
Shubham Panchal <panchalshubham826@gmail.com>
2022-03-13 17:42:19 +0530

    small bug fixes in pages/api/main_data

--- pages/api/main_data.js
+++ pages/api/main_data.js
@@ -1,8 +1,5 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default function handler(req, res) {
-  const { data } = await axios.get(
-    `https://labrecruit.herokuapp.com/volunteerSection/homePage?page=1&occupation=Labour`
-  );
-  res.send(data)
+  
 }
