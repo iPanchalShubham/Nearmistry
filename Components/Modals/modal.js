b5246bad7df3c6e7d744b98ef27ab3926b9a99c2
Shubham Panchal <panchalshubham826@gmail.com>
2022-04-14 15:39:20 +0530

    Modal update

--- Components/Modals/modal.js
+++ Components/Modals/modal.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ItemsImages from "../Static_components/ItemsImages";
-export default function Modal({ click, showModal,imgUrlArray }) {
+export default function Modal({ click, showModal, imgUrlArray }) {
   return (
     <>
       {showModal ? (
@@ -8,27 +8,30 @@ export default function Modal({ click, showModal,imgUrlArray }) {
           <div
             className="h-full w-full fixed top-0 right-0 z-40 bg-black opacity-50"
             onClick={click}
-          ></div>
-          <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
-            <div className="relative  mx-auto w-[300px]  lg:w-[400px]">
+          />
+          <div className="justify-center h-full  items-center flex overflow-x-hidden overflow-hidden fixed inset-0 z-50 outline-none focus:outline-none">
+            <div className="relative mx-auto lg:w-[500px] top-[6.8%] h-full w-full">
               {/*content*/}
-              <div className="border-0 rounded-sm shadow-lg relative flex flex-col  bg-gray-100 outline-none focus:outline-none">
+              <div className="border-0 rounded-sm shadow-lg relative flex flex-col bg-gray-100 outline-none focus:outline-none">
                 {/*header*/}
-                <div onClick={click} className="flex flex-row-reverse mr-2 text-xl cursor-pointer">
-                  x
+                <div
+                  onClick={click}
+                  className="flex justify-between"
+                >
+                  <div className="text-lg font-medium ml-2">Images</div>
+                  <div className="mr-2 text-xl cursor-pointer items-start">x</div>
                 </div>
-                <div className="h-[80%] flex items-start justify-between ml-2 border-b border-solid border-blueGray-200 rounded-t">
+                {/* <div className="h-full flex items-start justify-between ml-2 border-b border-solid border-blueGray-200 rounded-t">
                   <h3 className="text-2xl font-semibold">More Information</h3>
-                </div>
+                </div> */}
                 {/*body*/}
-                <div className="flex space-x-3 overflow-x-scroll scrollbar-hide p-3">
-                  {imgUrlArray?.map(( img, index) => (
-                  <ItemsImages img={img || selectedFile} key = {index} />
-                     ))
-                     } 
+                <div className="flex space-x-3 overflow-x-scroll scrollbar-hide p-3 bg-slate-800">
+                  {imgUrlArray?.map((img, index) => (
+                    <ItemsImages img={img || selectedFile} key={index} />
+                  ))}
                 </div>
                 {/*footer*/}
-                <div className="flex items-center justify-end p-6 border-t border-solid border-blueGray-200 rounded-b">
+                {/* <div className="flex items-center justify-end p-6 border-t border-solid border-blueGray-200 rounded-b">
                   <button
                     className="text-[#3f51b5] font-bold uppercase text-sm  mr-1 mb-1 ease-linear transition-all duration-150"
                     type="button"
@@ -36,7 +39,7 @@ export default function Modal({ click, showModal,imgUrlArray }) {
                   >
                     &#8592; Back
                   </button>
-                </div>
+                </div> */}
               </div>
             </div>
           </div>
